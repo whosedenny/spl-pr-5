fb65c37ab00ca0b3266e3526393fba95b912b880
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-11-03 14:42:15 +0200

    add functions from step 2 and add comments

--- TScripts/main.ts
+++ TScripts/main.ts
@@ -1,3 +1,4 @@
+//Базовий тип товару
 type BaseProduct = {
     id: number;
     name: string;
@@ -8,14 +9,26 @@ type BaseProduct = {
     type: string;
 };
 
+//Тип для товарів електроніки
 type Electronics  = BaseProduct & {
     category: 'electronics';
     warrantyPeriod: string;
     electricityConsumption: string;
 }
 
+//Тип для товарів одягу 
 type Clothing = BaseProduct & {
     category: "clothing";
     size: "XS" | "S" | "M" | "L" | "XL" | "XXL";
     gender: "M" | "W" | "M&W";
+};
+
+// Метод пошуку товару за його Id
+const findProduct = <T extends BaseProduct>(products: T[], id: number): T | undefined => {
+    return products.find((product) => product.id === id);
+};
+
+//Метод фільтрації товарів за мінімальною та максимальною ціною
+const filterByPrice = <T extends BaseProduct>(products: T[], maxPrice: number, minPrice: number = 0): T[] => {
+    return products.filter((product) => product.price >= minPrice && product.price <= maxPrice);
 };
