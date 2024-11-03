3d6fe3a2578f172aabab5e4e0c21a70e2fac90b6
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-11-03 15:00:05 +0200

    Add type for cart and 2 functions from step 3

--- TScripts/main.ts
+++ TScripts/main.ts
@@ -23,6 +23,12 @@ type Clothing = BaseProduct & {
     gender: "M" | "W" | "M&W";
 };
 
+//Тип для кошику
+type CartItem<T> ={
+    product: T,
+    quantity: number
+}
+
 // Метод пошуку товару за його Id
 const findProduct = <T extends BaseProduct>(products: T[], id: number): T | undefined => {
     return products.find((product) => product.id === id);
@@ -32,3 +38,30 @@ const findProduct = <T extends BaseProduct>(products: T[], id: number): T | unde
 const filterByPrice = <T extends BaseProduct>(products: T[], maxPrice: number, minPrice: number = 0): T[] => {
     return products.filter((product) => product.price >= minPrice && product.price <= maxPrice);
 };
+
+//Метод додавання товару до кошику
+const addToCart = <T extends BaseProduct>(cart: CartItem<T>[], product: T, quantity: number): CartItem<T>[] => {
+    const cartItem = cart.find(
+        (item) => item.product.id === product.id
+    );
+
+    if (cartItem) {
+        cartItem.quantity += quantity;
+    } else {
+        cart.push({ product, quantity });
+    }
+
+    return cart;
+};
+
+
+// Метод підрахунку загальної ціни товарів у кошику
+const calculateTotal = <T extends BaseProduct>(cart: CartItem<T>[]): number=>{
+    let res: number = 0;
+    
+    for (const item of cart) {
+        res += item.product.price * item.quantity;
+    }
+
+    return res;
+}
